ae6f69df3f75387a5b1f29dba7554391f8f41f77
Michael Waters <michael.j.waters@northwestern.edu>
2020-09-10 17:05:14 -0500

    I added a shifting target to compensate for chronic over and under reaching of energy.

--- contour_exploration.py
+++ contour_exploration.py
@@ -1,141 +1,292 @@
 import numpy as np
 
-from ase.md.md import MolecularDynamics
-
-from ase import units
+from ase.optimize.optimize import Dynamics
 from numpy.random import random
 
-class contour_exploration(MolecularDynamics):
-    def __init__(self, atoms, timestep=None,
+class contour_exploration(Dynamics):
+    def __init__(self, atoms, 
                 step_size = 0.5,
-                perpendicular_drift = 0.2,
-                parallel_step_scale = 1.8, 
-                remove_translation = True,
-                #max_parallel_fraction = 0.9,
+                parallel_drift = 0.2,
                 energy_target = None,
+                force_parallel_step_scale = None,
+                remove_translation = False,
+                use_FS = True,
+                initialize_old = True, initialization_step_scale = 1e-2,
+                use_target_shift = True, target_shift_previous_steps = 10,
                 seed = 60622,
+                verbose = False, 
                 trajectory=None, logfile=None,
-                loginterval=1, append_trajectory=False):
+                append_trajectory=False, loginterval=1):
 
-        '''Perpendicular drift breaks orbits like dimer form, so they spin on new axes
-parallel_step_scale over corrects the shift to target energy which greatly helps acheiveing the target since all the contours are curved. at 1.8 it reduces the deviation from the target energy by about a factor of 2.'''
-
+        '''Perpendicular drift breaks orbits like dimer form, so they spin on new axes'''
 
 
-            
-        self.step_size = step_size
-        self.parallel_step_scale = parallel_step_scale
-        self.perpendicular_drift = perpendicular_drift
+        if force_parallel_step_scale is None:
+            # a hureistic guess since most systems will overshoot when there is drift
+            FPSS = 0.9 + 0.5*parallel_drift
+        else:
+            FPSS = force_parallel_step_scale
+
+
+        self.verbose = verbose
         self.seed = seed
         self.remove_translation = remove_translation
+        self.use_FS = use_FS
+        #### for FS taylor expansion
+        self.T = None
+        self.Told = None
         
-        # we need velocities or this won't run and will produce NaNs,
-        # if none are provided we make random ones
-        p = atoms.get_momenta()
-        masses = atoms.get_masses()[:, np.newaxis]
-        v = p/masses
-        if self.dot(v,v) < 1e-6:
-            from ase import units
-            from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
-            MaxwellBoltzmannDistribution(atoms, 300 * units.kB)
-            print("No Velocities found, random ones applied")
+        self.N = None
+        self.Nold = None
+        
+        self.r_old = None
+        self.r     = None
 
-        from ase import units
-        timestep = 1.0 * units.fs # not used but MD needs it
-        MolecularDynamics.__init__(self, atoms, timestep, trajectory, logfile,
-                                   loginterval,
-                                   append_trajectory=append_trajectory,
-                                   )
-
+        #########
 
         if energy_target == None:
             self.energy_target = atoms.get_potential_energy()
         else:
             self.energy_target = energy_target
 
+        self.previous_energies = np.zeros(target_shift_previous_steps)
 
+        # we need velocities or this won't run and will produce NaNs,
+        # if none are provided we make random ones
+        p = atoms.get_momenta()
+        masses = atoms.get_masses()[:, np.newaxis]
+        v = p/masses
+        from ase import units
+        if self.dot(v,v) < 1e-6:
+            from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
+            MaxwellBoltzmannDistribution(atoms, 300 * units.kB)
+            print("No Velocities found, random ones applied")
+
+
+        if initialize_old:
+            self.step_size = step_size*initialization_step_scale
+            self.parallel_drift = 0.0
+            ## should this be 0.0 for a better initial curvature?
+            ## Or at least smaller than 1.0? doesn't seem to matter much
+            self.force_parallel_step_scale = 1.0 
+            self.use_target_shift = False
+            self.atoms = atoms
+            self.step()
+
+        self.step_size = step_size
+        self.parallel_drift = parallel_drift
+        self.force_parallel_step_scale = FPSS
+        self.use_target_shift = use_target_shift
+        
+        ## initizing the previous steps at the target energy slows 
+        ## target shifting untill the system has had 
+        ## 'baseline_shift_previous_steps' steps to equilibrate 
+        ## this should prevent occilations
+        self.previous_energies.fill(energy_target)
+        
+        print(self.previous_energies)
+        
+        ## loginterval exists for the MolecularDynamics classe but not the more
+        ## general Dynamics class
+        Dynamics.__init__(self, atoms,
+                                logfile, trajectory, #loginterval,
+                                append_trajectory=append_trajectory,
+                                )
+
+    ## Required stuff for Dynamics
+    def todict(self):
+        return {'type': 'contour-exploration',
+                'dyn-type': self.__class__.__name__,
+                'stepsize': self.step_size}
+
+    def irun(self, steps=50):
+        """ Call Dynamics.irun and adjust max_steps """
+        self.max_steps = steps + self.nsteps
+        return Dynamics.irun(self)
+
+
+    def run(self, steps=50):
+        """ Call Dynamics.run and adjust max_steps """
+        self.max_steps = steps + self.nsteps
+        return Dynamics.run(self)
+
+
+    def converged(self):
+        """ MD is 'converged' when number of maximum steps is reached. """
+        return self.nsteps >= self.max_steps
+
+
+    ### CED specific stuff
     def dot(self,a,b):
         '''a dot product for 3 column vectors of atoms.get_forces'''
         return (a*b).sum()
-        
+
     def vector_rejection(self,a,b):
         '''returns new vector that removes vector a's projection vector b'''
         aout = a - self.dot(a,b)/self.dot(b,b) *b
         return aout
 
+    def unit_vect(self,a):
+        return a / np.linalg.norm(a)
 
     def step(self, f=None):
-
+        debug = False
         atoms = self.atoms
 
         if f is None:
             f = atoms.get_forces()
 
+        self.r = atoms.get_positions()
+        current_energy = atoms.get_potential_energy()
 
+        ## update our history of self.previous_energies to include
+        ## our current energy
+        ## np.roll shifts the values to keep nice sequential ordering
+        self.previous_energies = np.roll(self.previous_energies,1) 
+        self.previous_energies[0] = current_energy
+        if self.verbose: print('Previous Energies' , self.previous_energies)
 
-            
+        if self.use_target_shift:
+            target_shift = self.energy_target - np.mean(self.previous_energies)
+            # does this help balance the small steps never reaching? nope. makes std worse
+            #target_shift = target_shift/self.force_parallel_step_scale
+            if self.verbose: print('Energy Target [Shift]:',self.energy_target,'[%f]'% target_shift)
+        else: 
+            target_shift = 0.0
+        deltaU = current_energy - ( self.energy_target + target_shift)
 
-        deltaU = atoms.get_potential_energy() - self.energy_target
-
+        ## get the velocity vector and old kinetic energy for momentum rescaling
         p = atoms.get_momenta()
+        KEold = atoms.get_kinetic_energy()
         masses = atoms.get_masses()[:, np.newaxis]
         v = p/masses
-        
-        f_over_norm_sqr = f / (np.linalg.norm(f)**2)
-        
+
+        ### get force an and correction distance
+        f_norm = np.linalg.norm(f)
+        self.N = self.unit_vect(f)
         # maybe reduce parallel step component to enhance stability?
-        w_parallel = deltaU*f_over_norm_sqr * self.parallel_step_scale 
+        delta_s_perpendicular =  (deltaU/f_norm)* self.force_parallel_step_scale # can be positive or negative
         
-        #print('v',v)
-        #print('f',f)
-        
-        
-        #w_perpendicular = v - (v*f).sum()*f_over_norm_sqr
-        w_perpendicular = self.vector_rejection(v,f)
-        ###
-        
-        w_drift = random((len(atoms),3))
-        w_drift = self.vector_rejection(w_drift,f)
-        w_drift = self.vector_rejection(w_drift,w_perpendicular)
-        w_drift = w_drift - w_drift.sum(axis = 0)/len(atoms) # removes wandering so systems don't wander
-        
-        w_drift_unit = w_drift / np.linalg.norm(w_drift) 
-        
-        
-        #print(self.dot(w_drift,w_perpendicular), self.dot(w_drift,f) )
-        
-        w_parallel_norm = np.linalg.norm(w_parallel)
-        w_perpendicular_unit = w_perpendicular/np.linalg.norm(w_perpendicular)
-        
-        if w_parallel_norm < self.step_size:
-            perpendicular_step_size = np.sqrt(self.step_size**2 - w_parallel_norm**2 )
-            
-            drift_vector         = perpendicular_step_size * self.perpendicular_drift * w_drift_unit
-            perpendicular_vector = perpendicular_step_size * np.sqrt(1- self.perpendicular_drift**2)*  w_perpendicular_unit
-            
-            dr = perpendicular_vector + drift_vector+ w_parallel
+        # remove velocity  projection on forces
+        w_parallel = self.vector_rejection(v,self.N)
+        self.T = self.unit_vect(w_parallel)
+        ### 
+
+        if self.Nold is None or self.use_FS == False: 
+            # we cannot apply the FS taylor expansion without a previous point
+            # so we do a dumb jump to start
+            w_perpendicular =  self.N * delta_s_perpendicular
+
+            # create drift unit with no projection on N or T
+            w_drift = random((len(atoms),3))
+            w_drift = self.vector_rejection(w_drift,self.N)
+            w_drift = self.vector_rejection(w_drift,self.T)
+            w_drift = w_drift - w_drift.sum(axis = 0)/len(atoms) # removes wandering so systems don't wander
+            D = self.unit_vect(w_drift)
+
+
+            if abs(delta_s_perpendicular) < self.step_size:
+                contour_step_size = np.sqrt(self.step_size**2 - delta_s_perpendicular**2 )
+
+                drift_vector    = contour_step_size * self.parallel_drift * D
+                parallel_vector = contour_step_size * np.sqrt(1- self.parallel_drift**2)*  self.T
+
+                dr = parallel_vector + drift_vector + w_perpendicular
+            else:
+                dr = self.step_size/abs(delta_s_perpendicular) * w_perpendicular
+
         else:
-            dr = self.step_size/w_parallel_norm * w_parallel
-        
-        
+
+             ####################  Frenet–Serret formulas
+            # this should keep the dr clear of the constraints
+            delta_r = self.r - self.rold
+            delta_s = np.linalg.norm(delta_r) 
+            # approximation of delta_s we use this incase an adaptive step_size algo get used 
+
+            delta_T = self.T - self.Told
+            delta_N = self.N - self.Nold
+            dTds = delta_T/delta_s
+            dNds = delta_N/delta_s
+            #kappa_T = np.linalg.norm(dTds)
+            kappa = np.linalg.norm(dNds) # normals are better since they are fixed to reality
+            Nfs = dTds/kappa
+
+            if debug:
+                print('Told\n' , self.Told)
+                print('T\n' , self.T)
+                
+                print('Nold\n', self.Nold)
+                print('N\n', self.N)
+                print('Nfs\n', Nfs)
+            
+            if self.verbose:
+                print('Curvature, kappa' , kappa, 'Radius, 1/kappa', 1/kappa)
+                print('T.Nfs', self.dot( self.T, Nfs), 'N.Nfs', self.dot(self.N,Nfs))
+            
+            
+            if abs(delta_s_perpendicular) < self.step_size:
+                contour_step_size = np.sqrt(self.step_size**2 - delta_s_perpendicular**2 )
+                delta_s_parallel = np.sqrt(1- self.parallel_drift**2) * contour_step_size
+                delta_s_drift    = contour_step_size*self.parallel_drift
+                
+                if self.verbose:
+                    print('step_size %f in (perpendicular), [parallel], {drift}: (%f) [%f] {%f}' % \
+                        (self.step_size, delta_s_perpendicular,delta_s_parallel, delta_s_drift))
+                
+                N_guess = self.N + dNds* delta_s_parallel
+                T_guess = self.T + dTds* delta_s_parallel
+                
+                w_perpendicular = delta_s_perpendicular*( N_guess)
+                
+                w_parallel = delta_s_parallel * self.T * (1 - (delta_s_parallel * kappa)**2/6.0) \
+                            + self.N * kappa/2 * delta_s_parallel**2
+                #########
+                drift = random((len(atoms),3))
+                drift = self.vector_rejection(drift,N_guess)
+                drift = self.vector_rejection(drift,T_guess)
+                # removes net translation, so systems don't wander
+                drift = drift - drift.sum(axis = 0)/len(atoms) 
+                D = self.unit_vect(drift)
+                w_drift = D*delta_s_drift
+                #########
+
+                if debug:
+                    print('Tguess\n', T_guess)
+                    print('Nguess\n', N_guess)
+                    print('w_perpendicular\n',w_perpendicular)
+                    print('w_parallel\n',w_parallel)
+                    print('w_drift\n',w_drift)
+
+
+                dr = w_perpendicular + w_parallel + w_drift
+                dr = self.step_size * self.unit_vect(dr) 
+                # because we guess our orthonormalization directions, 
+                # we should renormalize to ensure a correct step size 
+            else:
+                w_perpendicular =  self.N * delta_s_perpendicular
+                dr = self.step_size/abs(delta_s_perpendicular) * w_perpendicular
+            
+        ## now that dr is done, we check if there is translation
         if self.remove_translation:
             net_motion = dr.sum(axis = 0)/len(atoms)
             #print(net_motion)
             dr = dr - net_motion
             dr_unit = dr/np.linalg.norm(dr)
             dr = dr_unit*self.step_size
-        
-        
-        r = atoms.get_positions()
+
+
+
+        ##  save old positions before update
+        self.Told = self.T
+        self.Nold = self.N
+        self.rold = self.r
+
+        ### the update section ###
 
         # if we have constraints then this will do the first part of the
         # RATTLE algorithm:
-        atoms.set_positions(r + dr)
-        #if atoms.constraints:
-        #    p = (atoms.get_positions() - r) * masses / self.dt
-            
-        
-        p = (atoms.get_positions() - r) * masses #/ self.dt
+        atoms.set_positions(self.r + dr)
+        p = (atoms.get_positions() - self.r) * masses #/ self.dt
 
         # We need to store the momenta on the atoms before calculating
         # the forces, as in a parallel Asap calculation atoms may
@@ -143,13 +294,17 @@ parallel_step_scale over corrects the shift to target energy which greatly helps
         # migrate along with the atoms.
         atoms.set_momenta(p, apply_constraint=False)
 
+        ## Now we get the new forces! 
         f = atoms.get_forces(md=True)
         
 
-        # Second part of RATTLE will be done here:
-        #atoms.set_momenta(atoms.get_momenta() + 0.5 * self.dt * f)
+        ## rescaling momentum to maintain constant kinetic energy!
+        pnew = atoms.get_momenta()
+        KEnew = atoms.get_kinetic_energy()
+        Ms = np.sqrt(KEold/KEnew) #Ms = Momentum_scale
+        atoms.set_momenta(Ms*pnew)
+
+        ### Normally this would be the second part of RATTLE will be done here like this:
+        ### atoms.set_momenta(atoms.get_momenta() + 0.5 * self.dt * f)
+        if self.verbose: print('')
         return f
-
-
-
-
